3f2502c17cfa9bdfcd5815ba856661d2f09cf149
Vannos-Slid <ivan.kush05@gmail.com>
2026-04-23 19:13:57 +0300

    simple operators

--- js/operators.js
+++ js/operators.js
@@ -1,20 +1,39 @@
+const showBinEquestion = (strEquation, results) => {
+
+    console.log(`a = ${a}\nb = ${b}`)
+    let strPreShow = "";
+
+    if (!Array.isArray(results)) {
+        strPreShow = a + ` ${strEquation.at(0)} ` + b + " = " + results;
+    }
+    else {
+        const lenght = (strEquation.length < results.length) ?
+            showBinEquestion.length : results.length;
+
+        for (let index = 0; index < lenght; ++index) {
+            strPreShow += a + ` ${strEquation.at(index)} ` + b + " = " + results.at(index) + "\n";
+        }
+    }
+
+    console.log(strPreShow);
+}
+
+const showEquestion = (strEquation, result) => {
+    console.log(`a = ${a}\nb = ${b}`);
+    console.log(strEquation + " = " + result);
+}
+
 let a = 10;
 let b = 5;
 
-console.log(`a=${a}\nb=${b}\n+\n-\n*\n/\noperators`);
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+showBinEquestion("+-*/", [a + b, a - b, a * b, a / b])
 
 a = 11;
 b = 2
-console.log(`a=${a}\nb=${b}\n%\noperator`)
-
-console.log(a % b);
+showBinEquestion("%", a % b);
 
 a = 2;
 b = 4;
-console.log(`a=${a}\nb=${b}\n**\noperator`)
+showBinEquestion(["**"], a ** b);
 
-console.log(a ** b);
+showEquestion("a + b * b", a + b * b)
