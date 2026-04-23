562e14b4af68c21d1dde5588659080ed8e3b866d
Vannos-Slid <ivan.kush05@gmail.com>
2026-04-23 18:08:47 +0300

    primitive data types

---
+++ js/operators.js
@@ -0,0 +1,20 @@
+let a = 10;
+let b = 5;
+
+console.log(`a=${a}\nb=${b}\n+\n-\n*\n/\noperators`);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+a = 11;
+b = 2
+console.log(`a=${a}\nb=${b}\n%\noperator`)
+
+console.log(a % b);
+
+a = 2;
+b = 4;
+console.log(`a=${a}\nb=${b}\n**\noperator`)
+
+console.log(a ** b);
